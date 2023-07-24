00a833b1d1ff2099307303517281de9bb78bb5d5
phamquangduong07 <phamquangduong07@gmail.com>
2023-07-24 14:41:51 +0700

    Sắp xếp nổi bọt

---
+++ BubbleSort.java
@@ -0,0 +1,10 @@
+public class BubbleSort {
+    /*
+     * Sắp xếp nổi bọt
+     * Nguyên lý:
+     *          +, Chạy từ đầu đến cuối mảng
+     *          +, Nếu phần tử đứng  trước mà lớn hơn phân tử đứng sau thì đổi chỗ
+     *          +, Sau mỗi vòng lặp thì phần tử lớn nhất sẽ trôi xuông cuối mảng 
+     * 
+     */
+}
